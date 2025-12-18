afe9dcb19430988333363856f3d4dd8db191c3d3
rookie-leo <proleonardopa@gmail.com>
2025-12-18 16:19:15 -0300

    Componente React com TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App() {
+    console.log('Teste: testando...')
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Leonardo Blog</h1>
+            <p>
+                Olá, meu nome é Leonardo Paulino, seja muito bem-vindo
+                ao
+                meu blog pessoal! Aqui compartilho meus estudos,
+                experiências e ideias sobre tecnologia, sempre em
+                formato de
+                artigos diretos e práticos.
+                Fique à vontade para explorar o conteúdo e acompanhar
+                minha
+                evolução como desenvolvedor.
+                Para saber mais sobre mim, clique aqui.
+                Quer aprender como hospedei este blog usando GitHub
+                Pages?
+                Clique <a>aqui</a>.
+                Dicas e sugestões são bem-vindos! Deixe seu comentário
+                no meu
+                <a
+                    href="https://www.linkedin.com/in/leonardo-silva-paulino-6b3412191/"
+                    target="_blank">LinkedIn</a>
+            </p>
+        </>
+    );
 }
-
-export default App
