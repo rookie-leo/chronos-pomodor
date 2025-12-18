8c4e97791c9cbbddf2a475c17ae9cb20bd06d786
rookie-leo <proleonardopa@gmail.com>
2025-12-18 18:58:36 -0300

    Implementado um Component React para o Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 export function App() {
 
     return <>
-        <div className="container">
-            <div className='content'>
-                <section>
-                    LOGO
-                </section>
-            </div>
-        </div>
+        <Container>
+            <Heading>Logo</Heading>
+        </Container>
     </>;
 }
