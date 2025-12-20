0c6cf5413d73f76b1f64d5cfbd963a23e97abfd5
rookie-leo <proleonardopa@gmail.com>
2025-12-20 13:45:36 -0300

    Implementando componente menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import './styles/global.css';
 
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 export function App() {
 
     return <>
         <Container>
             <Logo />
+        </Container>
+
+        <Container>
+            <Menu />
         </Container>
     </>;
 }
