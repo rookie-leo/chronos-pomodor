40db4860747164afceb0bea69f470d13f63b98a9
rookie-leo <proleonardopa@gmail.com>
2025-12-20 13:54:18 -0300

    Implementando componente contador regressivo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 export function App() {
 
     return <>
@@ -13,6 +14,10 @@ export function App() {
 
         <Container>
             <Menu />
+        </Container>
+
+        <Container>
+            <CountDown />
         </Container>
     </>;
 }
