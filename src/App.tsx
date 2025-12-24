f9a62e1a1734f80bd7656aff69f33b2232e06ac4
rookie-leo <proleonardopa@gmail.com>
2025-12-24 12:15:26 -0300

    Componente GenericHtml para paginas que renderizam HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './styles/theme.css';
 import './styles/global.css';
 
+import { AboutPomodoro } from './pages/AboutPomodoro';
+import { NotFound } from './pages/NotFound';
 import { Home } from './pages/Home';
 
 export function App() {
     return <Home />;
+    // return <NotFound />;
+    // return <AboutPomodoro />;
 }
