88b4483e5fabb44f13f95d8d51e1ae4a5e95f02a
rookie-leo <proleonardopa@gmail.com>
2025-12-24 14:45:56 -0300

    Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,29 @@ import './styles/global.css';
 import { AboutPomodoro } from './pages/AboutPomodoro';
 import { NotFound } from './pages/NotFound';
 import { Home } from './pages/Home';
+import type { TaskStateModel } from './models/TaskStateModel';
+import { useState } from 'react';
+
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '00:00',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    }
+}
 
 export function App() {
-    return <Home />;
+    const [state, setState] = useState(initialState)
+
+    console.log('Testando Teste',state);
+
+
+    return <Home state={state} setState={setState}/>;
     // return <NotFound />;
     // return <AboutPomodoro />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,31 @@ import { MainForm } from '../../components/MainForm';
 import { MainTemplate } from '../../templates/MainTemplate';
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
+type HomeProps = {
+    state: TaskStateModel;
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
 
-export function Home() {
+export function Home(props: HomeProps) {
+    const { state, setState } = props;
+
+    function handleClick() {
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentCycle: 5,
+            };
+        });
+    }
+
     return (
         <MainTemplate>
+            <Container>
+                <button onClick={handleClick}>Clique aqui</button>
+            </Container>
+
             <Container>
                 <CountDown />
             </Container>
