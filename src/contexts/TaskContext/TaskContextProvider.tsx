f8a0fab5a1636fd4da1f343578975ea351600f16
rookie-leo <proleonardopa@gmail.com>
2025-12-31 13:54:32 -0300

    useReducer com objeto em state e actions

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import {  useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,20 +9,40 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
     const [state, setState] = useState(initialTaskState);
 
-    const [numero, dispatch] = useReducer((state, action) => {
+    type ActionType = {
+        type: string;
+        payload?: number;
+    }
+
+    const [myState, dispatch] = useReducer((state, action: ActionType) => {
         console.log(state, action)
 
-        switch (action) {
-            case "INCREMENT":
-                return state + 1
-            case "DECREMENT":
-                return state - 1
-            case "INITIAL_STATE":
-                return 0
+        switch (action.type) {
+            case "INCREMENT": {
+                if (!action.payload) return state
+                return {
+                    ...state,
+                    secondsRemaing: state.secondsRemaing + action.payload
+                }
+            }
+            case "DECREMENT": {
+                if (!action.payload) return state
+                return {
+                    ...state,
+                    secondsRemaing: state.secondsRemaing - action.payload
+                }
+            }
+            case "INITIAL_STATE": {
+                return {
+                    secondsRemaing: 0
+                }
+            }
         }
 
         return state;
-    }, 0)
+    }, {
+        secondsRemaing: 0,
+    })
 
     // useEffect(() => {
     //     console.log(state)
@@ -30,11 +50,12 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
     return (
         <TaskContext.Provider value={{ state, setState }}>
-            <h1>O numero é: {numero}</h1>
+            <h1>O estado é: {JSON.stringify(myState)}</h1>
 
-            <button onClick={() => dispatch("INCREMENT")}>Incrementar</button>
-            <button onClick={() => dispatch("DECREMENT")}>Decrementar</button>
-            <button onClick={() => dispatch("INITIAL_STATE")}>ZERAR</button>
+            <button onClick={() => dispatch({ type: "INCREMENT", payload: 10 })}>Incrementar + 10</button>
+            <button onClick={() => dispatch({ type: "INCREMENT", payload: 20 })}>Incrementar + 20</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", payload: 20 })}>Decrementar - 20</button>
+            <button onClick={() => dispatch({ type: "INITIAL_STATE"})}>ZERAR</button>
         </TaskContext.Provider>
     );
 }
