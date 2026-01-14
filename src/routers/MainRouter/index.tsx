30d0e5323673155faaf443b96e66ace432eaf544
rookie-leo <proleonardopa@gmail.com>
2026-01-14 06:49:10 -0300

    Implementando tabela de tarefas

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -3,6 +3,7 @@ import { Home } from '../../pages/Home';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { NotFound } from '../../pages/NotFound';
 import { useEffect } from 'react';
+import { History } from '../../pages/History';
 
 function ScrollToTop() {
     const { pathname } = useLocation()
@@ -18,6 +19,7 @@ export function MainRouter() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/history/' element={<History />} />
                 <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
